9c673e0416ab9049d0f3e5c4ddd07cd9d68def32
Torsten Fink <torsten.fink@akquinet.de>
2016-12-30 17:23:08 +0100

    create fat swarm jar

---
+++ mlc-wildflyswarm-project/mlc-wildflyswarm-microservice/src/main/java/de/akquinet/mlc/wildflyswarm/RestApplication.java
@@ -0,0 +1,8 @@
+package de.akquinet.mlc.wildflyswarm;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/")
+public class RestApplication extends Application {
+}
