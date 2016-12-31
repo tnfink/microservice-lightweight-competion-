8e4b18499c074829f22aa6027d7b55b76be8114c
Torsten Fink <torsten.fink@akquinet.de>
2016-12-31 16:56:28 +0100

    added Haskell project

---
+++ mlc-yesod-project/microservice.hs
@@ -0,0 +1,18 @@
+{-# LANGUAGE ExtendedDefaultRules #-}
+{-# LANGUAGE OverloadedStrings    #-}
+{-# LANGUAGE QuasiQuotes          #-}
+{-# LANGUAGE TemplateHaskell      #-}
+{-# LANGUAGE TypeFamilies         #-}
+import Yesod
+
+data App = App
+
+mkYesod "App" [parseRoutes|
+/ HomeR GET
+|]
+
+instance Yesod App
+
+getHomeR  = return $ object ["msg" .= "Hello World"]
+
+main = warp 3000 App
