fc4797b0829530bb8b5a19b3659b0e10b6b49c21
Torsten Fink <torsten.fink@akquinet.de>
2017-02-01 17:24:41 +0100

    added Go microservice

---
+++ mlc-go-project/mlc-go-microservice/src/adderMicroservice.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"strconv"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	summand1, err1 := strconv.ParseFloat(r.URL.Query().Get("summand1"), 64)
+
+	if err1 != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "summand1 not a float")
+		return
+	}
+
+	summand2, err2 := strconv.ParseFloat(r.URL.Query().Get("summand2"), 64)
+
+	if err2 != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		io.WriteString(w, "summand2 not a float")
+		return
+	}
+
+	sum := summand1 + summand2
+
+	io.WriteString(w, strconv.FormatFloat(sum, 'f', -1, 64))
+}
+
+func main() {
+	http.HandleFunc("/adder", handler)
+	http.ListenAndServe(":8080", nil)
+}
