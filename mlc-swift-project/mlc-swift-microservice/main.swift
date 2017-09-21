cd209be8a826eb09bef812a9445de6e91187f665
Torsten Fink <torsten.fink@akquinet.de>
2017-09-21 11:40:58 +0200

    - changed behavior to standard behavior

--- mlc-swift-project/mlc-swift-microservice/main.swift
+++ mlc-swift-project/mlc-swift-microservice/main.swift
@@ -1,15 +1,16 @@
 import Kitura
 
 let router = Router()
-router.get("/add/:summand1/and/:summand2") { request, response, next in
-    if let summand1 = Int(request.parameters["summand1"] ?? "0") {
-        if let summand2 = Int(request.parameters["summand2"] ?? "0") {
-            response.status(.OK).send("The sum of \(summand1) and \(summand2) equals \(summand1 + summand2).\n")
-            next()
-            return
-        }
+router.get("/adder") { request, response, next in
+    if let summand1Str = request.queryParameters["summand1"],
+       let summand1 = Float(summand1Str),
+       let summand2Str = request.queryParameters["summand2"],
+       let summand2 = Float(summand2Str) {
+            let result = summand1 + summand2
+            response.status(.OK).send("\(result)")
+    } else {
+       response.status(.badRequest).send("ERROR")
     }
-    response.status(.badRequest).send("ERROR: Cannot calculate with non integer values.\n")
     next()
 }
 
